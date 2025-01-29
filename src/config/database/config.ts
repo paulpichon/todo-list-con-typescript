dd800bd3a03d92b7955700095bcedd060d47ca68
Paul Pichon F <paul10_barca@hotmail.com>
2025-01-28 18:42:50 -0600

    feat: se añade un error personalizado con la clase AppError

--- src/config/database/config.ts
+++ src/config/database/config.ts
@@ -3,18 +3,23 @@
 import mongoose from 'mongoose';
 // Importacion de ZOD: conexion a la BD
 import { MONGODB_CONNECTION } from '../config';
+// Importamos nuestra clase para mostrar errores personalizados
+import AppError from '../../errors/CustomErrors';
 
 const databaseConnection = async (): Promise<void> => {
     // URI
     const uri: string = MONGODB_CONNECTION;
-    
     try {
         // Conectar a la BD
         await mongoose.connect(uri);
         console.log("La BD esta en linea");
         
     } catch (error) {
-        console.log("No se pudo conectar a la BD");
+        // Error personalizado
+        throw new AppError(
+            `Error al conectar a la base de datos: ${(error as Error).message}`,
+            500 
+        );
     }
 }
 // export
