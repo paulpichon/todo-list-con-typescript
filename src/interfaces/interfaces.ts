a7f98a998ff3a772f1e529ffb131297018681910
Paul Pichon F <paul10_barca@hotmail.com>
2025-02-06 21:50:35 -0600

    refactor: se modificar la forma de exportar las interfaces y se modifican el tipo de datos de alguna propiedades

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -3,12 +3,21 @@
 import mongoose from "mongoose";
 
 // Interface para schema Task
-export default interface Itask {
+interface Itask {
     _id?: mongoose.Types.ObjectId | string, // Para definir un ObjectId en una interfaz, usa mongoose.Types.ObjectId:
     nombre: string; //nombre de la tarea
-    status: string; //status de la tarea
+    status: 'pendiente' | 'completada'; //status de la tarea
     descripcion?: string; //descripcion de la tarea
-    prioridad: number; //prioridad de la tarea 1 = Importante, 2 = Algo importante, 3 = no importante
+    prioridad: 1 | 2 | 3; //prioridad de la tarea 1 = Importante, 2 = Algo importante, 3 = no importante
     fecha_creacion: Date;
     fecha_completado: Date;
+}
+// Interface de id: este id es el que viene de las rutas PUT y DELETE
+interface RequestParamsId {
+    id?: mongoose.Types.ObjectId | string,
+}
+// exports
+export {
+    Itask,
+    RequestParamsId
 }
