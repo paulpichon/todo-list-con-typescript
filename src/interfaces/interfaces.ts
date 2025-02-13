c72af415e632b1b6933d56989057340033b81112
Paul Pichon F <paul10_barca@hotmail.com>
2025-02-12 21:46:50 -0600

    refactor: se mueve interfaz: IRequestParamsGet, al archivo de interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 interface Itask {
     _id?: mongoose.Types.ObjectId | string, // Para definir un ObjectId en una interfaz, usa mongoose.Types.ObjectId:
     nombre: string; //nombre de la tarea
-    status: 'pendiente' | 'completada'; //status de la tarea
+    status: 'pendiente' | 'en-proceso' | 'completada'; //status de la tarea
     descripcion?: string; //descripcion de la tarea
     prioridad: 1 | 2 | 3; //prioridad de la tarea 1 = Importante, 2 = Algo importante, 3 = no importante
     fecha_creacion: Date;
@@ -16,8 +16,19 @@ interface Itask {
 interface RequestParamsId {
     id?: mongoose.Types.ObjectId | string,
 }
+
+// interfaz
+// Los valores de limite, desde y prioridad provienen de req.query, lo que significa que siempre serán cadenas de texto (string), incluso si representan números.
+interface IRequestParamsGet {
+    limite?: string; // Debe ser string porque req.query devuelve strings
+    desde?: string; // Debe ser string porque req.query devuelve strings
+    status?: 'pendiente' | 'en-proceso' | 'completada'; // Debe ser string porque req.query devuelve strings
+    prioridad?: string; // Debe ser string porque req.query devuelve strings
+}
+
 // exports
 export {
     Itask,
-    RequestParamsId
+    RequestParamsId,
+    IRequestParamsGet
 }
